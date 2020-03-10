ace1a48312890f033b6f1bc5987009787cbebdfe
huiboxes <huiboxes@gmail.com>
2020-03-10 22:40:55 +0800

    不可描述。。。

--- node.js/npm/npm.js
+++ node.js/npm/npm.js
@@ -3,13 +3,25 @@
     模块可以是任何一个文件或目录，只有能通过require函数加载的才是模块
     包是一个文件或目录（目录可以有多个文件）必须有一个Packages.json文件来描述才是包
 
-    package.json项目描述文件
+    package.json项目描述文件    package-lock.json
     元数据：描述自身的东西
     name（包的名字） 和 version（包的版本） 必须要有
     description（包的描述） author（作者）  dependencies（当前包依赖的其他包）
     main（入口文件） 从main字段指定的那个js文件开始执行
+    
 
-    package-lock.json 5以上才才会有，提升安装速度
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
